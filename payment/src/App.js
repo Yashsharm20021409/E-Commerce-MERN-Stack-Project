c3ce29dab48885d4f881731074588ff32c6cd5c0
Yash Sharma <yashsharma_20021409.cse@geu.ac.in>
2023-03-08 21:51:20 +0530

    payment page

--- payment/src/App.js
+++ payment/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import react from 'react';
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
+import Pay from './Components/Pay'
+import Success from './Components/Success'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/Pay" element={<Pay/>}></Route>
+          <Route exact path="/success" element={<Success/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ payment/src/Components/Success.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Success = () => {
+  return (
+    <div>
+      Hello I am Succes
+    </div>
+  )
+}
+
+export default Success
